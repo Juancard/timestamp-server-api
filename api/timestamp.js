097f1a716c5a70f6620e251a35eea8c087753a53
Juan Cardona <juancruz.23@hotmail.com>
2016-02-24 17:07:54 +0000

    Creado modulo timestamp en carpeta api

---
+++ api/timestamp.js
@@ -0,0 +1,17 @@
+var moment = require("moment");
+moment.locale("es");
+var formatoFechaNatural = "MMMM DD, YYYY";
+
+module.exports = function(res,query){
+    var unix = null;
+    var fechaNatural = null;
+    if (!isNaN(query) && moment(query, "X").isValid()){
+       unix = query;
+       fechaNatural = moment(query, "X").format(formatoFechaNatural);
+    } else if (moment(query, formatoFechaNatural).isValid()){
+       unix = moment(query, formatoFechaNatural).format("X");
+       fechaNatural = moment(query, formatoFechaNatural).format(formatoFechaNatural);
+    }
+    var json = JSON.stringify({"unix": (unix)? +unix : null, "natural":fechaNatural});
+    res.end(json);
+}
---
+++ server.js
@@ -0,0 +1,56 @@
+var http = require('http');
+var path = require('path');
+var fs = require("fs");
+var url = require("url");
+var mime = require("mime");
+var cache = {} // para guardar archivos leidos en memoria
+var timestamp = require("./api/timestamp.js");
+
+var server = http.createServer(function(req,res){
+   if (req.url == "/"){
+        var pathAbsoluto = "./client/index.html";
+        enviarArchivoStatic(res,cache,pathAbsoluto);
+   } else{
+        timestamp(res,req.url.slice(1));
+   }
+});
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
+
+//Para manejo de archivos de diversos tipos
+// funciones tomadas de Node.js in action.
+
+function enviar404(resp) {
+  resp.writeHead(404, {'Content-Type': 'text/plain'});
+  resp.write('Error 404: resource not found.');
+  resp.end();
+}
+function enviarArchivo(resp, archivoPath, archivoContenido) {
+  resp.writeHead(
+    200, 
+    {"content-type": mime.lookup(path.basename(archivoPath))}
+  );
+  resp.end(archivoContenido);
+}
+function enviarArchivoStatic(resp, cache, pathAbsoluto) {
+  if (cache[pathAbsoluto]) {
+    enviarArchivo(resp, pathAbsoluto, cache[pathAbsoluto]);
+  } else {
+    fs.exists(pathAbsoluto, function(exists) {
+      if (exists) {
+        fs.readFile(pathAbsoluto, function(err, data) {
+          if (err) {
+            enviar404(resp);
+          } else {
+            cache[pathAbsoluto] = data;
+            enviarArchivo(resp, pathAbsoluto, data);
+          }
+        });
+      } else {
+        enviar404(resp);
+      }
+    });
+  }
+}
